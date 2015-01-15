8053976b822c657662f37ad3e977450ab07912d2
Exequiel Pierotto <exequiel.pierotto@gmail.com>
2015-01-15 14:47:23 +0100

    Deleted OS support from metadata

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,3 @@ description      'Installs and Configures GitLab CE using the omnibus package'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-supports 'centos', ">= 6.6"
