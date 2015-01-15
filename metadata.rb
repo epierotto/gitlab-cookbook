66e82bcb679b590aae3c5f009e20847178b90c9d
Exequiel Pierotto <exequiel.pierotto@gmail.com>
2015-01-15 12:50:40 +0100

    First commit for new beggining

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'gitlab'
+maintainer       'Exequiel Pierotto'
+maintainer_email 'epierotto@abast.es'
+license          'Apache v2.0'
+description      'Installs and Configures GitLab CE using the omnibus package'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+supports 'centos', ">= 6.6"
